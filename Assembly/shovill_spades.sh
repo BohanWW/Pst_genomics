bcf27d2b0b7cfb74754a03b26c830585888418f5
BohanWW <116583777+BohanWW@users.noreply.github.com>
2025-08-15 11:11:45 -0600

    Create shovill_spades.sh

---
+++ Assembly/shovill_spades.sh
@@ -0,0 +1,2 @@
+#Script used for assembling short-read illumina hi-seq reads
+shovill --outdir ~/assembly/isolate --R1 ~/_raw/Yr-01-01_R1.fastq.gz --R2 ~/_raw/Yr-01-01_R2.fastq.gz --trim ON --assembler spades
