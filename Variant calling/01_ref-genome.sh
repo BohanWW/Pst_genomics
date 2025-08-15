19a3c557079143b2b0a00fea4283f50d162ae3b9
BohanWW <116583777+BohanWW@users.noreply.github.com>
2025-08-15 10:01:34 -0600

    Update 01_ref-genome.sh

--- Variant calling/01_ref-genome.sh
+++ Variant calling/01_ref-genome.sh
@@ -9,3 +9,11 @@ mamba activate samtools
 samtools faidx $1
 #Create .dict for reference
 gatk CreateSequenceDictionary -R $1
+
+#This file shows steps used to generate variant call files for an isolate
+#Run in dir where reference is located
+mamba activate bowtie2
+# $2 = reference genome
+#Map raw illumina reads of isolate to the reference; check path to fastq files before running
+#make the following codes as script “bowtie2.sh” and submit the job
+bowtie2 -p 32 -x $2 -1 Yr-01-09_trimmed_R1.fastq -2 Yr-01-09_trimmed_R2.fastq -S 01_Yr-01-09.sam (.gz files also work)
